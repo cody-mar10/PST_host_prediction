ddf80cb5573979231f2fd2c82b122cc95c64c79b
cody-mar10 <ccmartin6@wisc.edu>
2024-05-14 16:52:26 -0500

    add option for a dummy loader that returns the entire graph - instead of loading edges at a time - this is due to issues with `pyg-lib` on `chtc`

--- data.py
+++ data.py
@@ -3,6 +3,7 @@ from typing import Literal, Optional
 import lightning as L
 import torch
 from pydantic import BaseModel, Field, FilePath
+from torch.utils.data import DataLoader, Dataset
 from torch_geometric.data import HeteroData
 from torch_geometric.loader import LinkNeighborLoader
 from torch_geometric.transforms import RandomLinkSplit, ToUndirected
@@ -34,10 +35,44 @@ class DataConfig(BaseModel):
         [30, 30],
         description="number of edges to sample over n iterations (length of list) for single source edges that connect the same node types",
     )
+    dummy: bool = Field(False)
+
+
+class DummyDataset(Dataset):
+    """Store entire graph as a single dataset."""
+
+    data: HeteroData
+
+    def __init__(
+        self, file: Optional[FilePath] = None, data: Optional[HeteroData] = None
+    ):
+        if file is not None:
+            self.data = torch.load(file)
+        elif data is not None:
+            self.data = data
+        else:
+            raise ValueError("Either file or data must be provided.")
+
+        self._validate()
+
+    def _validate(self):
+        if not isinstance(self.data, HeteroData):
+            raise ValueError("Data must be a HeteroData object.")
+
+        if not self.data.is_undirected:
+            self.data = ToUndirected()(self.data)
+
+    def __len__(self):
+        return 1
+
+    def __getitem__(self, idx):
+        if idx > 0:
+            raise IndexError("Index out of range")
+        return self.data
 
 
 class GraphDataModule(L.LightningDataModule):
-    data: HeteroData
+    dataset: DummyDataset
     train_data: HeteroData
     val_data: HeteroData
 
@@ -56,15 +91,9 @@ class GraphDataModule(L.LightningDataModule):
 
         self.save_hyperparameters(config.model_dump())
 
-        self.data = torch.load(config.file)
+        self.dataset = DummyDataset(file=config.file)
+        self.data = self.dataset[0]
 
-        if not isinstance(self.data, HeteroData):
-            raise ValueError("Data must be a HeteroData object.")
-
-        if not self.data.is_undirected:
-            self.data = ToUndirected()(self.data)
-
-        # how make this configurable...?
         num_neighbors: dict[EdgeType, list[int]] = {}
         for edge in self.data.metadata()[1]:
             src, _, dst = edge
@@ -85,6 +114,10 @@ class GraphDataModule(L.LightningDataModule):
         self.neg_sampling_ratio = 1.0
 
     def setup(self, stage: Optional[Literal["fit", "test"]] = None):
+        # need to add negative examples to training data if using the dummy config
+        # which uses the entire graph at once
+        add_negative_train_samples = self.config.dummy
+
         if stage == "fit" or stage is None:
             splitter = RandomLinkSplit(
                 num_val=self.config.num_val,
@@ -94,40 +127,63 @@ class GraphDataModule(L.LightningDataModule):
                 disjoint_train_ratio=self.config.disjoint_train_ratio,
                 edge_types=[self.TEST_EDGE],
                 rev_edge_types=[self.REV_TEST_EDGE],
-                add_negative_train_samples=False,
+                add_negative_train_samples=add_negative_train_samples,
             )
             self.train_data, self.val_data, _ = splitter(self.data)
         elif stage == "test":
             # should probably negatively sample things here?
             self.test_data = self.data
 
-    def train_dataloader(self) -> LinkNeighborLoader:
-        train_dataloader = LinkNeighborLoader(
-            self.train_data,
-            **self.dataloader_kwargs,  # type: ignore
-            edge_label_index=(
-                self.TEST_EDGE,
-                self.train_data[self.TEST_EDGE].edge_label_index,
-            ),
-            edge_label=self.train_data[self.TEST_EDGE].edge_label,
-            neg_sampling_ratio=self.neg_sampling_ratio,
-            shuffle=True,
-        )
+    @staticmethod
+    def _collate(batch: list[HeteroData] | HeteroData):
+        if isinstance(batch, list) and len(batch) == 1:
+            return batch[0]
+        return batch
+
+    def train_dataloader(self) -> LinkNeighborLoader | DataLoader:
+        # TODO: should generate negative edges on the fly still for dummy loader?
+        if not self.config.dummy:
+            train_dataloader = LinkNeighborLoader(
+                self.train_data,
+                **self.dataloader_kwargs,  # type: ignore
+                edge_label_index=(
+                    self.TEST_EDGE,
+                    self.train_data[self.TEST_EDGE].edge_label_index,
+                ),
+                edge_label=self.train_data[self.TEST_EDGE].edge_label,
+                neg_sampling_ratio=self.neg_sampling_ratio,
+                shuffle=True,
+            )
+        else:
+            train_dataloader = DataLoader(
+                DummyDataset(data=self.train_data),
+                batch_size=1,
+                shuffle=True,
+                collate_fn=self._collate,
+            )
 
         return train_dataloader
 
-    def val_dataloader(self) -> LinkNeighborLoader:
-        val_dataloader = LinkNeighborLoader(
-            self.val_data,
-            **self.dataloader_kwargs,  # type: ignore
-            edge_label_index=(
-                self.TEST_EDGE,
-                self.val_data[self.TEST_EDGE].edge_label_index,
-            ),
-            edge_label=self.val_data[self.TEST_EDGE].edge_label,
-            neg_sampling_ratio=None,
-            shuffle=False,
-        )
+    def val_dataloader(self) -> LinkNeighborLoader | DataLoader:
+        if not self.config.dummy:
+            val_dataloader = LinkNeighborLoader(
+                self.val_data,
+                **self.dataloader_kwargs,  # type: ignore
+                edge_label_index=(
+                    self.TEST_EDGE,
+                    self.val_data[self.TEST_EDGE].edge_label_index,
+                ),
+                edge_label=self.val_data[self.TEST_EDGE].edge_label,
+                neg_sampling_ratio=None,
+                shuffle=False,
+            )
+        else:
+            val_dataloader = DataLoader(
+                DummyDataset(data=self.val_data),
+                batch_size=1,
+                shuffle=False,
+                collate_fn=self._collate,
+            )
 
         return val_dataloader
 
